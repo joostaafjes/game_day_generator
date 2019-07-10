f45d8211790a88975bdf4f78b47d8a867cee7434
joost aafjes <joostaafjes@gmail.com>
2019-07-10 23:54:45 +0200

    try multiple possibilities....recursive

--- generate.py
+++ generate.py
@@ -15,40 +15,62 @@ def check_overlap(rounds, combination):
 
 def check_team_combination_already_exists(rounds, combination):
    for exist_round in rounds:
-      for exist_game_idx in range(len(exist_round) / 2):
-         for comb_game_idx in range(len(combination) / 2):
+      for exist_game_idx in range(0, len(exist_round), 2):
+         for comb_game_idx in range(0, len(combination), 2):
+            if combination[comb_game_idx] == no_of_teams - 2:
+               continue
             if exist_round[exist_game_idx] == combination[comb_game_idx] and \
                exist_round[exist_game_idx + 1] == combination[comb_game_idx + 1]:
                   return True
+            if exist_round[exist_game_idx] == combination[comb_game_idx + 1] and \
+                    exist_round[exist_game_idx] == combination[comb_game_idx + 1]:
+               return True
    return False
 
-no_of_rounds = 6
+def find_next_round(round_id, rounds):
+   if round_id == no_of_rounds:
+      return rounds
+   # combinations = list(itertools.permutations(range(no_of_teams - 2)))
+   combinations = list(itertools.permutations(range(no_of_teams)))
+   for combination_tuple in combinations:
+      combination = list(combination_tuple)
+      # combination.insert(round_id * 2, no_of_teams - 1)
+      # combination.insert(round_id * 2, no_of_teams - 2)
+      # first round
+      if len(rounds) == 0:
+         print('ronde {}: {}'.format(round_id, rounds + [combination]))
+         return find_next_round(round_id + 1, rounds + [combination])
+      # other rounds
+      if check_overlap(rounds, combination) or check_team_combination_already_exists(rounds, combination):
+         continue
+      print('ronde {}: {}'.format(round_id, rounds + [combination]))
+      next = find_next_round(round_id + 1, rounds + [combination])
+      if next:
+         return next
+      else:
+         print('dead end...')
+         continue
+
+   return None
+
+no_of_rounds = 5
+# no_of_teams = no_of_rounds * 2 - 2
 no_of_teams = no_of_rounds * 2
 
 rounds = []
 
 start = time.time()
 
-for round_id, round in enumerate(range(no_of_rounds)):
-   combinations = itertools.permutations(range(no_of_teams))
-   for combination in combinations:
-      # first round
-      if len(rounds) == 0:
-         rounds.append(combination)
-         print('ronde {}: {}'.format(round_id, combination))
-         break
-      # other rounds
-      if check_overlap(rounds, combination) or check_team_combination_already_exists(rounds, combination):
-         continue
-      rounds.append(combination)
-      print('ronde {}: {}'.format(round_id, combination))
-      break
+rounds = find_next_round(0, rounds)
+
 
 print('took {} seconds'.format(time.time() - start))
 
 print('----------------------')
 print('final solution')
 print('----------------------')
-for round_id, round in enumerate(rounds):
-   print('ronde {}: {}'.format(round_id, round))
-
+if rounds:
+    for round_id, round in enumerate(rounds):
+       print('ronde {}: {}'.format(round_id, round))
+else:
+   print('no solution found')
